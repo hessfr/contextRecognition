6c8abe4a2241e241a81791a53819f55caf3f5118
hessfr <frederik.hess@gmail.com>
2014-05-06 14:57:56 +0200

    Delete methods: paramSweepPCA, countIdentifiedOutliers and visualize

--- findOutliers.py
+++ findOutliers.py
@@ -9,99 +9,6 @@ from pprint import pprint
 from sklearn.decomposition import PCA
 import pylab as pl
 import ipdb as pdb #pdb.set_trace()
-
-def countIdentifiedOutliers(fileFeatureDict,lof):
-    """
-    After manually inserting outlier files of a different context class, this method counts how many of these files were correctly
-    identified as outliers
-    @param fileFeatureDict: Dictionary containing an array of the features for every file name
-    @param lof: Local Outlier Factor object containing results of the local outlier factor computations
-    """
-    
-    """ Set up array with ground truth: """
-    #outliersGT = ["10-05-31 Duesseldorf Mittagessen Restaurant Prickynoo.wav", "0078 Restaurant_cutlery.wav", "AMB_Int_Restaurant_Pizzeria_001.wav"]    
-    outliersGT = ["06-2011 Freight Train Pass #2.wav", "Novosibirsk_subway_1.wav", "steam train.wav"]
-    
-    keys = fileFeatureDict.keys()
-    y = []
-    
-    for key in keys:
-        if key in outliersGT:
-            y.append(0)
-        else:
-            y.append(1)
-    
-    cnt = 0
-    
-    for outlier in lof:
-        if fileFeatureDict.keys()[outlier["index"]] in outliersGT:
-            cnt=cnt+1
-    
-    print "Total number of outliers: " + str(len(lof)) + " - number of correct outliers: " + str(cnt)
-
-def visualize(fileFeatureDict, lof):
-    """
-    Use a PCA to plot the files in 2D space and mark manually inserted outliers in a different color
-    @param fileFeatureDict: Dictionary containing an array of the features for every file name
-    @param lof: lof object containing results of the local outlier factor computations
-    """
-    pca = PCA(n_components=2)
-    X_r = pca.fit(fileFeatureDict.values()).transform(fileFeatureDict.values())
-    
-    print len(fileFeatureDict.values()[0])
-    print X_r.shape
-    
-    #outliersGT = ["10-05-31 Duesseldorf Mittagessen Restaurant Prickynoo.wav", "0078 Restaurant_cutlery.wav", "AMB_Int_Restaurant_Pizzeria_001.wav"]    
-    outliersGT = ["06-2011 Freight Train Pass #2.wav", "Novosibirsk_subway_1.wav", "steam train.wav"]
-    
-    keys = fileFeatureDict.keys()
-    y = []
-    
-    for key in keys:
-        if key in outliersGT:
-            y.append(0)
-        else:
-            y.append(1)
-
-    pl.figure()
-    
-    pl.scatter(X_r[:,0],X_r[:,1],c=y, s=150)
-    
-    pl.show()
-
-def paramSweepPCA(folderName, paramRange, nComponents, fileFeatureDict=None):
-    """
-    Calculate Local Outlier Factor (LOF) for all files in the given folder by using a PCA of mean values of the 12 MFCC features and
-    sweep through different values of the minPts parameter
-    @param folderName: Name of the folder in the "sound" folder, i.e. if you want to folder the folder ./sound/car give "car" a folderName
-    @param paramRange: Range for the minPts parameter as list
-    @param nComponents: Defines the number of components for the PCA
-    @param fileFeatureDict: Dictionary containing an array of the features for every file name. If defined, these features will be used and not
-    extracted from the given folder
-    """
-
-    if fileFeatureDict == None:
-        fileFeatureDict = extractFeatures(folderName)
-    
-    """ Build list of the mean values: """
-    values = fileFeatureDict.values()
-    PCAList = []
-    
-    pca = PCA(n_components=nComponents)
-    X_r = pca.fit(fileFeatureDict.values()).transform(fileFeatureDict.values())
-    
-    for entry in values:
-        PCAList.append(tuple(entry))
-    
-    if(fileFeatureDict is not None):
-        
-        for minPts in paramRange:
-            print "------ minPts = " + str(minPts) + "---------"
-            lof = outliers(minPts, PCAList)
-            for outlier in lof:
-                print "File " + str(fileFeatureDict.keys()[outlier["index"]]) + " has LOF of " + str(outlier["lof"]) 
-            print "Total number of outliers: " + str(len(lof))
-            #countIdentifiedOutliers(fileFeatureDict,lof)
         
 def paramSweep(folderName, paramRange):
     """
@@ -128,7 +35,6 @@ def paramSweep(folderName, paramRange):
                 print "File " + str(fileFeatureDict.keys()[outlier["index"]]) + " has LOF of " + str(outlier["lof"])
             
             print "Total number of outliers: " + str(len(lof))
-            #countIdentifiedOutliers(fileFeatureDict,lof)
  
 def LOF_Folder(folderName, minPts):
     """
