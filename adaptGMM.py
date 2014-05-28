Added calculation of posteriors in adaptGMM.py and corrected scaling mistake in simulateAL.py

adaptGMM.py
@@ -32,6 +32,8 @@ def adaptGMM(trainedGMM, updatePoints, label, nSteps=100):
 
     posteriors_old = newGMM["posteriors"][int(label)]
 
+    # print(newGMM["posteriors"][int(label)])
+
     n_train_old = math.ceil(posteriors_old.sum())
     n_train_new = X.shape[0]
 
@@ -42,6 +44,8 @@ def adaptGMM(trainedGMM, updatePoints, label, nSteps=100):
     means = copy.deepcopy(old_means)
     covars = copy.deepcopy(old_covars)
     weights = copy.deepcopy(old_weights)
+
+    converged = False
 
     #pdb.set_trace()
 
@@ -95,21 +99,40 @@ def adaptGMM(trainedGMM, updatePoints, label, nSteps=100):
         logLik = np.mean(np.log(F))
 
         if abs((logLik/float(prevLogLik)) - 1) < loglik_threshold:
-            newGMM["clfs"][int(label)].converged_ = True
-            print("EM converged")
+            converged = True
             break
 
         # pdb.set_trace()
 
         prevLogLik = logLik
 
-    print(str(i) + " loop cycles")
+    if converged == True:
+        print("EM algorithm converged after " + str(i) + " cycles")
+    else:
+        print("EM algorithm not converged after " + str(i) + " cycle. Increase nSteps parameter to fix this problem")
 
+    """ Calculate posterior probabilities for the adapted class and update them in the classifier dict: """
+    # calculate the probabilities:
+    proba = np.zeros((n_train_new, n_components))
+    for c in range(n_components):
+        proba[:,c] = pdf(X, means[c,:], covars[c])
+
+    # calculate the responsibilities:
+    responsibilities = np.zeros((n_train_new, n_components))
+    for j in range(n_train_new):
+        responsibilities[j,:] = (weights * proba[j,:]) / np.sum(weights * proba[j,:]) # Noch EPS addieren??????
+
+    posteriors = responsibilities.sum(axis=0) # shape = n_components
+
+    newGMM["posteriors"][int(label)] = posteriors
+
+    """ update all other model parameters in the classifier dict: """
     newGMM["clfs"][int(label)].weights_ = weights
     newGMM["clfs"][int(label)].means_ = means
     newGMM["clfs"][int(label)].covars_ = covars
+    newGMM["clfs"][int(label)].converged_ = converged
 
-    # pdb.set_trace()
+    # print(newGMM["posteriors"][int(label)])
 
     return newGMM
 

classifiers.py
@@ -98,20 +98,16 @@ def trainGMM(featureData):
 
         """ Calculate posterior probabilities (for each component), as we need to know them to adapt the model later: """
         proba = np.zeros((int(n_tmp), n_comp))
-        # proba = np.zeros((int(X_train.shape[0]), n_comp)) # over completely all points, or only over all points of that class??
         for c in range(n_comp):
             proba[:,c] = pdf(tmpTrain, tmpClf.means_[c,:], tmpClf.covars_[c])
-            # proba[:,c] = pdf(X_train, tmpClf.means_[c,:], tmpClf.covars_[c]) # over completely all points, or only over all points of that class??
 
         # calculate the responsibilities:
         responsibilities = np.zeros((int(n_tmp), n_comp))
         for j in range(int(n_tmp)):
             responsibilities[j,:] = (tmpClf.weights_ * proba[j,:]) / np.sum(tmpClf.weights_ * proba[j,:])
 
-        """ Calculate posterior probabilities: """
+        # calculate the posterior probabilities:
         posteriors = responsibilities.sum(axis=0) # shape = n_components
-
-        print(posteriors)
 
         posteriorList.append(posteriors)
 

simulateAL.py
@@ -11,6 +11,7 @@ from sklearn import preprocessing
 from classifiers import getIndex
 from classifiers import testGMM
 from featureExtraction import FX_multiFolders
+from adaptGMM import adaptGMM
 import ipdb as pdb #pdb.set_trace()
 
 _thresholdDict = {}
@@ -62,32 +63,24 @@ def simulateAL(trainedGMM, testFeatureData):
     y_GT = createGTUnique(trainedGMM['classesDict'], testFeatureData.shape[0], 'labelsAdapted.txt')
     y_GTMulti = createGTMulti(trainedGMM["classesDict"],testFeatureData.shape[0], 'labels.txt')
 
-    """ Get all Features to retrain the GMM later (only tentitative, as incremental model update not implemented yet: """
-    featureDataFS = FX_multiFolders(["Conversation","Office","TrainInside"]) #TODO: implement this properly!!!
-
-    labelData = featureDataFS["labels"]
-
-    featureData = trainedGMM['scaler'].transform(featureDataFS["features"])
-
     print(trainedGMM["classesDict"])
-    print(featureDataFS["classesDict"])
 
     """ Create index arrays to define which elements are used to evaluate performance and which for simulation of the AL
     behavior: """
     [evalIdx, simIdx] = splitData(y_GT)
 
-    testFeatureData = trainedGMM['scaler'].transform(testFeatureData) # apply the feature scaling from the training phase
-
     evalFeatures = testFeatureData[evalIdx == 1]
     evalLabels = y_GTMulti[evalIdx == 1] # contains multiple labels for each point
 
     simFeatures = testFeatureData[simIdx == 1]
+    simFeatures = trainedGMM['scaler'].transform(simFeatures)
     simLabels = y_GT[simIdx == 1]
+
     """ simLabels contains unique label for each point that will be used to update the model later,
     e.g. if a point has the labels office and conversation, it will be trained with conversation (accoring to the provided
     groundTruthLabels """
 
-    currentGMM = dict(trainedGMM)
+    currentGMM = copy.deepcopy(trainedGMM)
 
     allGMM = []
     allGMM.append(currentGMM)
@@ -126,8 +119,6 @@ def simulateAL(trainedGMM, testFeatureData):
                     updatePoints = tmpFeatures[tmpLabels == currentLabel]
                     labelAccuracy.append(1)
 
-                    pdb.set_trace()
-
                 else:
                     # updatePoints = simFeatures[0:i,:]
                     # labelAccuracy.append(checkLabelAccuracy(simLabels[0:i], currentLabel))
@@ -143,18 +134,14 @@ def simulateAL(trainedGMM, testFeatureData):
 
                 givenLabels.append(currentLabel)
 
-                print(featureData.shape)
-                print(labelData.shape)
-                print(updatePoints.shape)
-
-
-
-                currentGMM, featureData, labelData = adaptGMM(currentGMM, updatePoints, currentLabel, featureData, labelData)
+                currentGMM = adaptGMM(currentGMM, updatePoints, currentLabel, nSteps=100)
                 allGMM.append(currentGMM)
                 timestamps.append(i*0.032)
 
+                y_pred = testGMM(currentGMM, evalFeatures, useMajorityVote=True, showPlots=False)
+
         #for testing:
-        if len(allGMM) == 3:
+        if len(allGMM) == 4:
             print("Stopped loop for testing")
             break
 
@@ -205,7 +192,7 @@ def queryCriteria(trainedGMM, featurePoint, className, criteria="entropy"):
             tmpProduct[i] = likelihoodNormed[i] * logLikelihoodNormed[i]
         entropy = tmpProduct.sum(axis=0) * -1
 
-        if entropy > threshold:
+        if entropy > 1.0: #threshold: #xxxx
             return True
         else:
             return False
@@ -228,7 +215,7 @@ def queryCriteria(trainedGMM, featurePoint, className, criteria="entropy"):
         else:
             return False
 
-def adaptGMM(trainedGMM, featurePoints, currentLabel, featureData, labelData):
+def adaptGMM_OUTDATED(trainedGMM, featurePoints, currentLabel, featureData, labelData):
     """
     Incorporate new data point into the GMM model
     @param trainedGMM:
@@ -258,8 +245,6 @@ def adaptGMM(trainedGMM, featurePoints, currentLabel, featureData, labelData):
 
     tmpTrain = X_train[iTmp]
 
-    #pdb.set_trace()
-
     """ Use expectation-maximization to fit the Gaussians: """
     print("adapting GMM")
     clf.fit(tmpTrain)
@@ -273,8 +258,6 @@ def adaptGMM(trainedGMM, featurePoints, currentLabel, featureData, labelData):
     labelData = y_train
 
     print("adapting GMM finished")
-
-    #pdb.set_trace()
 
     return newGMM, featureData, labelData
 
@@ -293,7 +276,7 @@ def evaluateGMM(trainedGMM, evalFeatures, evalLabels):
     """
 
     @param trainedGMM:
-    @param evalFeatures:
+    @param evalFeatures: not scaled, as scaling is done in testGMM method
     @param evalLabels: Ground truth label array with multiple labels per data points
     @return:
     """
@@ -303,7 +286,6 @@ def evaluateGMM(trainedGMM, evalFeatures, evalLabels):
     y_pred = testGMM(trainedGMM, evalFeatures, useMajorityVote=True, showPlots=False)
 
     n_classes = len(trainedGMM["classesDict"])
-    agreementCounter = 0
     validCounter = 0
     delIdx = [] #list of indexes of the rows that should be deleted
     correctlyPredicted = [0] * n_classes #list to count how often each class is predicted correctly