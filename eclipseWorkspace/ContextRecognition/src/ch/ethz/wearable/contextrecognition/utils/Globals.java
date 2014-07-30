75ed258e77d73072f3118a5ee5571ebc5b28dc75
hessfr <frederik.hess@gmail.com>
2014-07-30 08:15:28 +0200

    ..

--- eclipseWorkspace/ContextRecognition/src/ch/ethz/wearable/contextrecognition/utils/Globals.java
+++ eclipseWorkspace/ContextRecognition/src/ch/ethz/wearable/contextrecognition/utils/Globals.java
@@ -84,7 +84,7 @@ public class Globals {
 	
 	// Array of context classes that can be selected initially:
 	public static String[] initialContextClasses = {"Office", "Conversation", "Restaurant",
-		"Car", "Bus", "Train", "Street", "Vacuuming", "Toilet", "Kitchen"}; //TODO: include sleeping...
+		"Car", "Bus", "Train", "Street", "Vacuum", "Toilet", "Kitchen"}; //TODO: include sleeping...
 	// Array indicating if the context class should be selected initially:
 	public static Boolean[] defaultClasses = {true, true, true, true, true,
 		true, true, true, true, true};
