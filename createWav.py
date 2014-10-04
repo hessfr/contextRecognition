e9c9ab019440b55b4274fa0469b3e4c471ce6a5e
hessfr <frederik.hess@gmail.com>
2014-10-04 13:44:01 +0200

    Added createWav.py from server_small_scale_exp branch

---
+++ createWav.py
@@ -0,0 +1,60 @@
+import numpy as np
+import os
+import math
+import wave
+import struct
+import ipdb as pdb
+
+def createWav(path, binary_file, output_file, sample_rate=16000, datatype=np.int16):
+    """
+
+    """
+    fin = open(os.path.join(path, binary_file), "rb")
+
+    fin.seek(0, os.SEEK_END)
+    file_length = fin.tell()
+    len_seconds = fin.tell() / (sample_rate * 2)
+
+    #pdb.set_trace()
+
+    stepsize_seconds = 600
+    stepsize = int(sample_rate * stepsize_seconds)
+    
+    n_iter = int(math.ceil(len_seconds/float(stepsize_seconds)))
+
+    output_file = output_file + ".wav"
+    fout = wave.open(os.path.join(path, output_file), 'wb')
+    fout.setparams((1, 2, sample_rate, 0, 'NONE', 'not compressed'))
+    
+    for i in range(n_iter):
+
+        offset_seconds = i*stepsize_seconds
+        offset = int(sample_rate * 2 * offset_seconds)
+        fin.seek(offset, os.SEEK_SET)
+
+        data = np.fromfile(fin, datatype, stepsize)
+
+        data_size = data.shape[0]
+
+        data_struct = struct.pack('%sh' % data_size, *data)
+        
+        try:
+            fout.writeframes(data_struct)
+        except:
+            print("Writing of data element failed, skipping this element")
+
+    fout.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
